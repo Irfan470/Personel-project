46edeedb21232e8d2eeba3cd4ff8a862a94ff845
Irfan470 <amonium7@gmail.com>
2023-03-08 12:26:51 +0100

    added sign in sign up and forgot password pages

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function ForgotPassword() {
-  return <div>Forgot Password</div>;
+  const [email, setEmail] = useState("");
+  function onChange(event) {
+    setEmail(event.target.value);
+  }
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password </h1>
+      <div className="flex flex-wrap justify-center items-center px-6 py-12 max-w-6xl">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6 mx-auto ">
+          <img
+            src="https://images.unsplash.com/photo-1577373644244-ff9935a13a2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=442&q=80"
+            alt="keys"
+            className="w-[75%] ml-40 rounded-2xl"
+          />
+        </div>
+        <div
+          className="w-full md:w-[67%] lg:w-[40%] lg:ml-20
+          "
+        >
+          <form>
+            <input
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white rounded transition  ease-in-out mb-6"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+            ></input>
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p>
+                Don't have an account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p className="mb-6">
+                <Link
+                  to="/sign-in"
+                  className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out"
+                >
+                  Sign in instead?
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="justify-center w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Send reset password
+            </button>
+            <div className="my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 my-4 after:border-t flex after:flex-1 items-center after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsEyeSlash, BsEye } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
-  return <div>Sign In</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  function onChange(event) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }));
+  }
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In </h1>
+      <div className="flex flex-wrap justify-center items-center px-6 py-12 max-w-6xl">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6 mx-auto ">
+          <img
+            src="https://images.unsplash.com/photo-1577373644244-ff9935a13a2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=442&q=80"
+            alt="keys"
+            className="w-[75%] ml-40 rounded-2xl"
+          />
+        </div>
+        <div
+          className="w-full md:w-[67%] lg:w-[40%] lg:ml-20
+          "
+        >
+          <form>
+            <input
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white rounded transition  ease-in-out mb-6"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+            ></input>
+            <div className="relative ">
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white rounded transition  ease-in-out mb-5"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="password"
+              />
+              {showPassword ? (
+                <BsEyeSlash
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              ) : (
+                <BsEye
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              )}
+            </div>
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p>
+                Don't have an account?
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p className="mb-6">
+                <Link
+                  to="/forgot-password"
+                  className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out"
+                >
+                  Forgot password?
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="justify-center w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Sign In
+            </button>
+            <div className="my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 my-4 after:border-t flex after:flex-1 items-center after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
