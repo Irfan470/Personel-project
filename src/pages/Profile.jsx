c6efafc496497d1f870f2129be33b289af5334c0
Irfan470 <amonium7@gmail.com>
2023-03-11 17:20:51 +0100

    create listin UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
+import { GrMapLocation } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const auth = getAuth();
@@ -90,6 +92,19 @@ export default function Profile() {
               </p>
             </div>
           </form>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800"
+          >
+            <Link
+              to="/create-listing"
+              className="flex justify-center items-center"
+            >
+              <GrMapLocation className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />{" "}
+              sell ot rent
+            </Link>
+          </button>
         </div>
       </section>
     </>
